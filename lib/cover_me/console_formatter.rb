4abcdcbf2680bcae8df7ec3dd52572180b4deec8
KITAITI Makoto <KitaitiMakoto@gmail.com>
2011-08-23 22:35:52 +0900

    Make configurable whether use color and whether output verbosely

--- lib/cover_me/console_formatter.rb
+++ lib/cover_me/console_formatter.rb
@@ -7,18 +7,24 @@ class CoverMe::ConsoleFormatter < CoverMe::Formatter
 
   def format_index(index)
     template_file = File.join(File.dirname(__FILE__), 'templates', 'console.erb')
-    tuc = Test::Unit::Color
-    colors = {
-      'reset' => (tuc.new('reset')).escape_sequence,
-      'hit'   => (tuc.new('green',  foreground: false) +
-                  tuc.new('white',  bold: true)).escape_sequence,
-      'near'  => (tuc.new('yellow', bold: true) +
-                  tuc.new('black',  foreground: false)).escape_sequence,
-      'miss'  => (tuc.new('red',    foreground: false) +
-                  tuc.new('white',  bold: true)).escape_sequence
-    }
+    config = CoverMe.config.console_formatter
 
-    puts ERB.new(File.read(template_file), nil, '-').result(binding)
+    if config.use_color
+      tuc = Test::Unit::Color
+      colors = {
+        'reset' => (tuc.new('reset')).escape_sequence,
+        'hit'   => (tuc.new('green',  foreground: false) +
+                    tuc.new('white',  bold: true)).escape_sequence,
+        'near'  => (tuc.new('yellow', bold: true) +
+                    tuc.new('black',  foreground: false)).escape_sequence,
+        'miss'  => (tuc.new('red',    foreground: false) +
+                    tuc.new('white',  bold: true)).escape_sequence
+      }
+    else
+      colors = {}
+    end
+
+    $stdout.print ERB.new(File.read(template_file), nil, '-').result(binding)
   end
 
 end
