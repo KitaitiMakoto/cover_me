1e0b51db8d79606d889b4bd818d4f5b5643c0fa5
KITAITI Makoto <KitaitiMakoto@gmail.com>
2011-08-28 04:46:16 +0900

    Make not render duplicated line

--- lib/cover_me/console_formatter.rb
+++ lib/cover_me/console_formatter.rb
@@ -24,20 +24,38 @@ class CoverMe::ConsoleFormatter < CoverMe::Formatter
       colors = {}
     end
 
+    # To do:
+    #   Here is the presentation logic instead of business logic
     template('console.erb', '-').run(binding)
-    unless index.percent_tested == 100
-      index.reports.sort.each do |report|
-        template('console.file.erb', '-').run(binding)
-        next unless config.verbose
-        report.coverage.each_with_index do |count, i|
-          if count == 0
-            sl = report.source.length
-            sls = sl.to_s.length
-            template('console.verbose.erb', '-').run(binding)
-          end
+    return if index.percent_tested == 100
+
+    index.reports.sort.each do |report|
+      template('console.file.erb', '-').run(binding)
+      next unless config.verbose
+      $stdout.puts '    Untested line(s):' unless report.executed_percent == 100.0
+      sl = report.source.length
+      sls = sl.to_s.length
+      last_rendered = nil
+      report.coverage.each_with_index do |count, i|
+        next unless count == 0
+
+        unless last_rendered == i - 1
+          line = i - 1
+          status = report.coverage[i - 1] == 0 ? 'miss' : 'hit'
+          template('console.line.erb', '-').run(binding)
+
+          line = i
+          status = report.coverage[i] == 0 ? 'miss' : 'hit'
+          template('console.line.erb', '-').run(binding)
         end
+
+        line = i + 1
+        status = report.coverage[i + 1] == 0 ? 'miss' : 'hit'
+        template('console.line.erb', '-').run(binding)
+
+        $stdout.puts "" unless report.coverage[i + 1] == 0
+        last_rendered = i
       end
     end
   end
-
 end
