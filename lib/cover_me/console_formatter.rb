b62cb6ffd5fd66eb6e6383b42702dd78ec0ef19f
KITAITI Makoto <KitaitiMakoto@gmail.com>
2011-08-28 05:54:33 +0900

    [REFACTOR]Extract methodd

--- lib/cover_me/console_formatter.rb
+++ lib/cover_me/console_formatter.rb
@@ -2,60 +2,47 @@ require 'test/unit/color'
 
 class CoverMe::ConsoleFormatter < CoverMe::Formatter
 
+  Color = Test::Unit::Color
+
   def format_report(report)
   end
 
   def format_index(index)
     template_file = File.join(File.dirname(__FILE__), 'templates', 'console.erb')
     config = CoverMe.config.console_formatter
+    colors = config.use_color ? self.colors : {}
 
-    if config.use_color
-      tuc = Test::Unit::Color
-      colors = {
-        'reset' => (tuc.new('reset')).escape_sequence,
-        'hit'   => (tuc.new('green',  foreground: false) +
-                    tuc.new('white',  bold: true)).escape_sequence,
-        'near'  => (tuc.new('yellow', bold: true) +
-                    tuc.new('black',  foreground: false)).escape_sequence,
-        'miss'  => (tuc.new('red',    foreground: false) +
-                    tuc.new('white',  bold: true)).escape_sequence
-      }
-    else
-      colors = {}
-    end
-
-    # To do:
-    #   Here is the presentation logic instead of business logic
     template('console.erb', '-').run(binding)
     return if index.percent_tested == 100
 
     index.reports.sort.each do |report|
       template('console.file.erb', '-').run(binding)
       next unless config.verbose
+
       $stdout.puts '    Untested line(s):' unless report.executed_percent == 100.0
-      sl = report.source.length
-      sls = sl.to_s.length
+      sls = (report.source.length + 1).to_s.length
       last_rendered = nil
-      report.coverage.each_with_index do |count, i|
-        next unless count == 0
+      report.coverage.each_with_index do |count, line|
+        next unless report.coverage[line - 1] == 0 || count == 0 || report.coverage[line + 1] == 0
+        # $stdout.puts '' unless count == 0 || report.coverage[line - 1] == 0
 
-        unless last_rendered == i - 1
-          line = i - 1
-          status = report.coverage[i - 1] == 0 ? 'miss' : 'hit'
-          template('console.line.erb', '-').run(binding)
-
-          line = i
-          status = report.coverage[i] == 0 ? 'miss' : 'hit'
-          template('console.line.erb', '-').run(binding)
-        end
-
-        line = i + 1
-        status = report.coverage[i + 1] == 0 ? 'miss' : 'hit'
+        status = count == 0 ? 'miss' : 'hit'
         template('console.line.erb', '-').run(binding)
-
-        $stdout.puts "" unless report.coverage[i + 1] == 0
-        last_rendered = i
+        $stdout.puts '' unless count == 0 || report.coverage[line + 1] == 0
       end
+      $stdout.puts '' if config.verbose && index.percent_tested == 100
     end
+  end
+
+  def colors
+    {
+      'reset' => (Color.new('reset')).escape_sequence,
+      'hit'   => (Color.new('green',  foreground: false) +
+                  Color.new('white',  bold: true)).escape_sequence,
+      'near'  => (Color.new('yellow', bold: true) +
+                  Color.new('black',  foreground: false)).escape_sequence,
+      'miss'  => (Color.new('red',    foreground: false) +
+                  Color.new('white',  bold: true)).escape_sequence
+    }
   end
 end
